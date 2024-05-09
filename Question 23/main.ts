89fbcdd68b69a17dbf950d99a83e9bda65469b7d
muhammad_saleem <160356353+Mrsaleem110@users.noreply.github.com>
2024-05-09 11:55:18 +0500

    adding 8 day of challenge

---
+++ Question 23/main.ts
@@ -0,0 +1,40 @@
+// *Question 23:* Conditional Tests: Write a series of conditional tests. Predict the results of each test.
+
+// *Explain & TIP:* Conditional tests check if something is true or false. Writing different tests helps you understand how conditions work in programming.
+// 1nd condition of ture and false
+let car ="Honda";
+console.log("if car == 'Honda' ?I predict true")
+console.log(car=='Honda') //True condition
+
+console.log("if car == 'Toyota' ?I predict false")
+console.log(car=='Toyota') //false condition
+
+// 2nd condition of ture and false
+let bike="unique";
+console.log("if bike == 'unique' ?I predict true")
+console.log(bike=='unique') //True condition
+
+console.log("if bike== 'crown' ?I predict false")
+console.log(bike=='crown') //false condition
+
+// 3th condition of ture and false
+let system="computer";
+console.log("if system== 'computer' ?I predict true")
+console.log(system=='computer') //True condition
+
+console.log("if system== 'laptop' ?I predict false")
+console.log(system== 'laptop' ) //false condition
+// 4th condition of ture and false
+let home="banglow";
+console.log("if home == 'banglow' ?I predict true")
+console.log(home=='banglow') //True condition
+
+console.log("if home== 'plote' ?I predict false")
+console.log(home=='plote') //false condition
+// 5th condition of ture and false
+let animal="lion";
+console.log("if animal == 'lion' ?I predict true")
+console.log(animal=='lion') //True condition
+
+console.log("if animal== 'tiger' ?I predict false")
+console.log(animal=='tiger') //false condition
---
+++ Question 24/app.ts
@@ -0,0 +1,29 @@
+
+// *Question 24:* More Conditional Tests: Expand your conditional tests to cover a wider range of comparisons.
+
+// *Explain & TIP:* Experiment with different types of conditions to see how they work. This builds a deeper understanding of logic in programming.
+// Equality with strings
+//Equality with strings
+console.log("Testing equality with strings:");
+console.log("apple" == "apple"); // True
+console.log("apple" !="apple"); // False
+
+// Using the lower case function
+console.log("Testing with lower case:");
+console.log("Apple".toLowerCase() == "apple"); // True
+
+// Numerical tests
+console.log("Numerical tests:");
+console.log(10 > 5); // True
+console.log(2 < 1); // False
+
+// Tests using "and" and "or" operators
+console.log("Tests with 'and' and 'or':");
+console.log(true && false); // False
+console.log(true || false); // True
+
+// Test whether an item is in a array
+let fruits = ["apple", "banana", "cherry"];
+console.log("Is 'banana' in fruits?");
+console.log(fruits.includes("banana")); // True
+console.log(fruits.includes("stawberry")); // false
